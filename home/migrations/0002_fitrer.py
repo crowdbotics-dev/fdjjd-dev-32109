f362f9b3a670f7fd4c0bde7bb91e8b01db62cef5
Crowdbotics <engineering@crowdbotics.com>
2022-09-06 05:26:11 +0000

    Update models

--- home/migrations/0002_fitrer.py
+++ home/migrations/0002_fitrer.py
@@ -8,16 +8,24 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('home', '0001_load_initial_data'),
+        ("home", "0001_load_initial_data"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Fitrer',
+            name="Fitrer",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('bgdhdh', models.TextField()),
-                ('fdgdeyee', models.BigIntegerField(blank=True, null=True)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("bgdhdh", models.TextField()),
+                ("fdgdeyee", models.BigIntegerField(blank=True, null=True)),
             ],
         ),
     ]
