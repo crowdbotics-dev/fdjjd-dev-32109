75df775d7b8e4d5c6387265fdea6bc8b9bbf532a
Crowdbotics <engineering@crowdbotics.com>
2022-09-06 05:25:36 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Fitrer
+
+admin.site.register(Fitrer)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,11 @@
+from django.conf import settings
 from django.db import models
+
+
+class Fitrer(models.Model):
+    "Generated Model"
+    bgdhdh = models.TextField()
+    fdgdeyee = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
